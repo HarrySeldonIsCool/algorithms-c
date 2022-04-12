e7b819e6d91313809fc1e6db5639449215c6692e
HarrySeldonIsCool <83349404+HarrySeldonIsCool@users.noreply.github.com>
2022-04-12 21:02:19 +0200

    Update sqrt_pow.c

--- sqrt_pow.c
+++ sqrt_pow.c
@@ -18,14 +18,15 @@ float mypow(float a, float pow){
     float out = 1.0;
     __uint32_t bitpow = *(__uint32_t*)&pow;
     __uint32_t point = bitpow >> 23 & 0xff;
+    bitpow = (bitpow & 0x7fffff)+0x800000ů
     float inter = a;
     for (size_t i = 0x7f; i < point; i++){
-        if(((bitpow & 0x7fffff)+0x800000) >> (point > i+23 ? 31 : 23+i-point) & 1) out *= inter;
+        if(bitpow >> (point > i+23 ? 31 : 23+i-point) & 1) out *= inter;
         inter *= inter;
     }
     inter = a;
     for (size_t i = 0x7f; i > point-23; i--){
-        if(((bitpow & 0x7fffff)+0x800000) >> 23+i-point & 1) out *= inter;
+        if(bitpow >> 23+i-point & 1) out *= inter;
         inter = mysqrt(inter);
     }
     return out;
